58f7c133038846b6ea7d30bb74716cfafcc3894a
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-03-14 20:51:45 +0530

    Add dist directory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './style.css';
-/* eslint-disable radix */
+
 // Get the necessary HTML elements
-// const scoreContainer = document.getElementById('score-container');
 const scoresDiv = document.getElementById('scores');
 const refreshButton = document.querySelector('.refresh button');
 const submitButton = document.querySelector('.input-scores button');
@@ -9,7 +8,7 @@ const nameInput = document.getElementById('name');
 const scoreInput = document.getElementById('score');
 
 // Store the scores in an array
-let scores = [];
+const scores = [];
 
 // Function to update the scores displayed on the page
 const updateScores = () => {
@@ -17,23 +16,24 @@ const updateScores = () => {
   for (let i = 0; i < scores.length; i += 1) {
     const score = scores[i];
     const div = document.createElement('div');
-    if (scores[i] % 2 === 0) {
-      score.style.background = 'grey';
+    if (score.score % 2 === 0) {
+      div.style.background = 'grey';
     }
-    div.innerHTML = `${score.name}: ${score.score}<hr class = "dash">`;
+    div.innerHTML = `${score.name}: ${score.score}<hr class="dash">`;
     scoresDiv.appendChild(div);
   }
 };
 
 // Function to handle the refresh button click
 const handleRefresh = () => {
-  scores = []; // Remove all the scores
+  scores.length = 0; // Remove all the scores
   updateScores();
 };
+
 // Function to handle the submit button click
 const handleSubmit = () => {
   const name = nameInput.value.trim();
-  const score = parseInt(scoreInput.value.trim());
+  const score = parseInt(scoreInput.value.trim(), 10);
 
   if (name && score) { // Check that both inputs are valid
     scores.push({ name, score });
