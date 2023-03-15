4f0dfb4f4f729c88ba5dfe46f27fa5416eb04780
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-03-15 12:41:32 +0530

    Add leaderboard API

--- src/index.js
+++ src/index.js
@@ -1,48 +1,77 @@
 import './style.css';
+/* eslint-disable no-console */
+/* eslint-disable arrow-body-style */
+const API = {
+  createGame: async () => {
+    return { gameId: 'scores' };
+  },
+  getScores: async (gameId) => {
+    const scores = JSON.parse(localStorage.getItem(gameId)) || [];
+    return { scores };
+  },
+  submitScore: async (gameId, name, score) => {
+    const scores = JSON.parse(localStorage.getItem(gameId)) || [];
+    scores.push({ name, score });
+    localStorage.setItem(gameId, JSON.stringify(scores));
+    return { scores };
+  },
+};
 
-// Get the necessary HTML elements
 const scoresDiv = document.getElementById('scores');
 const refreshButton = document.querySelector('.refresh button');
 const submitButton = document.querySelector('.input-scores button');
 const nameInput = document.getElementById('name');
 const scoreInput = document.getElementById('score');
 
-// Store the scores in an array
-const scores = [];
+let gameId;
+let scores = [];
 
-// Function to update the scores displayed on the page
 const updateScores = () => {
-  scoresDiv.innerHTML = ''; // Clear the previous scores
+  scoresDiv.innerHTML = '';
   for (let i = 0; i < scores.length; i += 1) {
     const score = scores[i];
     const div = document.createElement('div');
-    if (score.score % 2 === 0) {
-      div.style.background = 'grey';
-    }
     div.innerHTML = `${score.name}: ${score.score}<hr class="dash">`;
     scoresDiv.appendChild(div);
   }
 };
 
-// Function to handle the refresh button click
-const handleRefresh = () => {
-  scores.length = 0; // Remove all the scores
-  updateScores();
-};
-
-// Function to handle the submit button click
-const handleSubmit = () => {
-  const name = nameInput.value.trim();
-  const score = parseInt(scoreInput.value.trim(), 10);
-
-  if (name && score) { // Check that both inputs are valid
-    scores.push({ name, score });
-    nameInput.value = '';
-    scoreInput.value = '';
+const handleRefresh = async () => {
+  try {
+    const response = await API.getScores(gameId);
+    scores = response.scores;
     updateScores();
+  } catch (error) {
+    console.error(error);
   }
 };
 
-// Add event listeners to the buttons
+const handleSubmit = async () => {
+  const name = nameInput.value.trim();
+  const score = parseInt(scoreInput.value.trim(), 10);
+
+  if (name && score) {
+    try {
+      await API.submitScore(gameId, name, score);
+      scores = [];
+      nameInput.value = '';
+      scoreInput.value = '';
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
+
+const createGame = async () => {
+  try {
+    const response = await API.createGame('My Game');
+    gameId = response.gameId;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+createGame();
+
 refreshButton.addEventListener('click', handleRefresh);
 submitButton.addEventListener('click', handleSubmit);
